b229e67e95f8edce08aa7a295f21707967fae446
Chayma <werheni.chayma@gail.com>
2020-10-22 15:00:53 +0100

    version 3

--- src/bookstore/Models/Etudiant.java
+++ src/bookstore/Models/Etudiant.java
@@ -10,5 +10,39 @@ package bookstore.Models;
  * @author Chayma
  */
 public class Etudiant {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Etudiant(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
     
 }

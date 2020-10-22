0851b552da680c25d2082d37241d38e8ede8e6e9
Chayma <werheni.chayma@gail.com>
2020-10-22 15:39:44 +0100

    version 4

--- src/bookstore/Models/Book.java
+++ src/bookstore/Models/Book.java
@@ -14,4 +14,27 @@ import java.util.Date;
 public class Book {
     String authors[];
     Date publicationDate;
+
+    public Book(String[] authors, Date publicationDate) {
+        this.authors = authors;
+        this.publicationDate = publicationDate;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+    
+    
 }
